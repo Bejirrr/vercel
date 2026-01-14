6f7eac38b3c77d9d90ead2b439ec7b7aa8c67261
Bejirrr <animelive24jam@gmail.com>
2026-01-15 00:34:36 +0700

    Add files via upload

---
+++ testingzx/api/raw.js
@@ -0,0 +1,35 @@
+import { initDb, getScript, getAllScripts } from "./db.js";
+
+await initDb();
+
+export default async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+
+  if (req.method === "OPTIONS") return res.status(200).end();
+
+  try {
+    const name = req.url?.split("/").pop();
+    if (!name) return res.status(400).send("Error: Invalid request");
+
+    // If requesting public-scripts list
+    if (name === "public-scripts") {
+      const scripts = await getAllScripts();
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(scripts);
+      return;
+    }
+
+    const script = await getScript(name);
+    if (!script) {
+      return res.status(404).send("Script not found");
+    }
+
+    const cleanCode = script.content.replace(/\u200D/g, '');
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.status(200).send(cleanCode);
+  } catch (err) {
+    console.error("Raw error:", err);
+    res.status(500).send("Server error");
+  }
+};
